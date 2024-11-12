a65ed2bec4e7e6fda7c4fc3efbef57934fbfd5ce
EMMADEKO10 <emmanueldeko64@gmail.com>
2024-11-12 17:21:26 +0100

    "Update allowedOrigins.js: replaced 'https://diasporium.vercel.app' with 'https://brievents.vercel.app'"

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -5,7 +5,7 @@ const allowedOrigins = [
     "http://localhost:3800",
     "https://scpt-postoffice-box.vercel.app",
     "http://localhost:3700",
-    "https://diasporium.vercel.app"
+    "https://brievents.vercel.app"
   ];
   
   module.exports = allowedOrigins;
